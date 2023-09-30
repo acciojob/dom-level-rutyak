78596c0f12cc62d3b193616cd3af5304daf24a56
rutyak <132219103+rutyak@users.noreply.github.com>
2023-09-30 14:23:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+// Get the element with the ID "level"
+
+const bottomEle = document.getElementById("level");
+
+let domLevel = 0;
+
+
+let parentEle = bottomEle;
+
+while( parentEle !== document.documentElement){
+
+    parentEle = parentEle.parentElement;
+
+    domLevel++;
+}
+
+alert(`The level of the element is: ${domLevel}`)
